d11731c8834eebf4c80b49a37d086d1e1868a031
Rahul <rahul.bohara2016@gmail.com>
2021-09-23 20:20:48 +1000

    build the frontend

--- app.js
+++ app.js
@@ -17,12 +17,13 @@ const app = express();
 app.use(cors());
 
 app.use(express.json());
-
+console.log(path.resolve(__dirname, "./client/build"));
+console.log(path.resolve(__dirname, "./client/build", "index.html"));
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.resolve(__dirname, "./client/build")));
 
   app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "./client/build", "index.html"))
+    res.sendFile(path.resolve(__dirname, "./client/build/", "index.html"))
   );
 } else {
   app.get("/", (req, res) => {
